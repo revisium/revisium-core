c4d14a0ec0328731f018c74ac4395a3757135ef6
Anton Kashirov <anton.kashirov@gmail.com>
2024-12-30 21:54:38 +0300

    feat: export database module and prisma service (#12)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { CoreModule } from 'src/core/core.module';
+export { DatabaseModule } from 'src/database/database.module';
+export { PrismaService } from 'src/database/prisma.service';
 export { notificationEventEmitter } from 'src/notification/notification-event-emitter';
