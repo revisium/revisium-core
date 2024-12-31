b4dbd3e2784578fd1c2e5bd3697f5e692215ae7b
anton62k <anton.kashirov@gmail.com>
2024-12-31 13:12:08 +0300

    refactor: added AdminModule from revisium repository

---
+++ src/admin/envjs.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ApiExcludeController } from '@nestjs/swagger';
+
+@ApiExcludeController()
+@Controller()
+export class EnvJsController {
+  constructor(private configService: ConfigService) {}
+
+  @Get('env.js')
+  envJs() {
+    const envs = {
+      REACT_APP_ENDPOINT_HOST: this.configService.get<string>(
+        'REACT_APP_ENDPOINT_HOST',
+      ),
+      REACT_APP_ENDPOINT_PORT: this.configService.get<string>(
+        'REACT_APP_ENDPOINT_PORT',
+      ),
+    };
+
+    return `window.__env__ = ${JSON.stringify(envs)}`;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { CoreModule } from 'src/core/core.module';
 export { MetricsApiModule } from 'src/metrics-api/metrics-api.module';
+export { AdminModule } from 'src/admin/admin.module';
+export { AdminModuleOptions } from 'src/admin/admin-module.options';
 export { notificationEventEmitter } from 'src/notification/notification-event-emitter';
