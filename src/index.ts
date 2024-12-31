3bd71a6fd7a3362c40dd92f3f64c90458429b7b6
Anton Kashirov <anton.kashirov@gmail.com>
2024-12-31 13:31:23 +0300

    refactor: add admin modules, move InMemoryServer (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { CoreModule } from 'src/core/core.module';
 export { MetricsApiModule } from 'src/metrics-api/metrics-api.module';
-export { notificationEventEmitter } from 'src/notification/notification-event-emitter';
+export { AdminModule } from 'src/admin/admin.module';
+export { AdminModuleOptions } from 'src/admin/admin-module.options';
+export { InMemoryServer } from 'src/notification/in-memory-server';
