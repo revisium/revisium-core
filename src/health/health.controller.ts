d1e2cdc044c9b75737cabdda6a38f590ac45c6c3
Anton Kashirov <anton.kashirov@gmail.com>
2025-01-01 22:31:26 +0300

    feat: liveness and readiness (#23)

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,12 +1,37 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController, ApiTags } from '@nestjs/swagger';
+import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
+import type { HealthIndicatorFunction } from '@nestjs/terminus/dist/health-indicator';
+import { NotificationCheckService } from 'src/health/notification-check.service';
+import { PrismaCheckService } from 'src/health/prisma-check.service';
 
 @ApiExcludeController()
 @ApiTags('health')
 @Controller('health')
 export class HealthController {
-  @Get()
-  get() {
-    return 'ok';
+  constructor(
+    private readonly health: HealthCheckService,
+    private readonly prisma: PrismaCheckService,
+    private readonly notifications: NotificationCheckService,
+  ) {}
+
+  @Get('liveness')
+  @HealthCheck()
+  liveness() {
+    const indicators: HealthIndicatorFunction[] = [
+      async () => this.prisma.check(),
+    ];
+
+    if (this.notifications.available) {
+      indicators.push(async () => this.notifications.check());
+    }
+
+    return this.health.check(indicators);
+  }
+
+  @Get('readiness')
+  @HealthCheck()
+  readiness() {
+    return this.health.check([]);
   }
 }
--- src/health/health.module.ts
+++ src/health/health.module.ts
@@ -1,5 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TerminusModule } from '@nestjs/terminus';
+import { DatabaseModule } from 'src/database/database.module';
 import { HealthController } from 'src/health/health.controller';
+import { NotificationCheckService } from 'src/health/notification-check.service';
+import { PrismaCheckService } from 'src/health/prisma-check.service';
 
-@Module({ controllers: [HealthController] })
+@Module({
+  imports: [DatabaseModule, ConfigModule, TerminusModule],
+  controllers: [HealthController],
+  providers: [PrismaCheckService, NotificationCheckService],
+})
 export class HealthModule {}
