ff6577c48e5462af151b2b131507a2fe11827a3a
Anton Kashirov <anton.kashirov@gmail.com>
2025-01-03 08:17:22 +0300

    chore: removed @casl/prisma

--- src/auth/casl-ability.factory.ts
+++ src/auth/casl-ability.factory.ts
@@ -1,49 +1,21 @@
-import { AbilityBuilder, PureAbility } from '@casl/ability';
-import { createPrismaAbility, PrismaQuery, Subjects } from '@casl/prisma';
+import { AbilityBuilder, createMongoAbility, MongoQuery } from '@casl/ability';
+import { AnyObject } from '@casl/ability/dist/types/types';
 import { Injectable } from '@nestjs/common';
-import {
-  Organization,
-  Project,
-  Branch,
-  Revision,
-  Table,
-  Row,
-  Endpoint,
-  User,
-} from '@prisma/client';
-import { PermissionAction, UserRole } from 'src/auth/consts';
+import { UserRole } from 'src/auth/consts';
 import { PrismaService } from 'src/database/prisma.service';
-
-export type AppSubjects = Subjects<{
-  Organization: Organization;
-  Project: Project;
-  Branch: Branch;
-  Revision: Revision;
-  Table: Table;
-  Row: Row;
-  Endpoint: Endpoint;
-  User: User;
-}>;
-
-export type AppAbility = PureAbility<
-  [PermissionAction, AppSubjects],
-  PrismaQuery
->;
 
 @Injectable()
 export class CaslAbilityFactory {
   constructor(private readonly prisma: PrismaService) {}
 
   async createAbility(...roles: UserRole[]) {
-    const { can, build } = new AbilityBuilder<AppAbility>(createPrismaAbility);
+    const { can, build } = new AbilityBuilder(createMongoAbility);
 
     for (const role of roles) {
       for (const { action, subject, condition } of await this.getPermissions(
         role,
       )) {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        can(action, subject as unknown as AppSubjects, condition);
+        can(action, subject, condition as MongoQuery<AnyObject>);
       }
     }
 
