0392cebb441d496ca32f4d8d4bdc4c424f56add8
Anton Kashirov <anton.kashirov@gmail.com>
2025-01-03 15:26:32 +0300

    chore: user queries tests (#32)

---
+++ src/user/queries/handlers/__tests__/get-user-organization.spec.ts
@@ -0,0 +1,87 @@
+import { CqrsModule, QueryBus } from '@nestjs/cqrs';
+import { QueryHandlerType } from '@nestjs/cqrs/dist/query-bus';
+import { Test, TestingModule } from '@nestjs/testing';
+import { Organization, Prisma } from '@prisma/client';
+import { nanoid } from 'nanoid';
+import { testCreateUser } from 'src/__tests__/create-models';
+import { UserOrganizationRoles } from 'src/auth/consts';
+import { PrismaService } from 'src/database/prisma.service';
+import { GetUserOrganizationQuery } from 'src/user/queries/impl';
+import { GetUserOrganizationHandler } from '../get-user-organization.handler';
+
+describe('GetUserOrganizationHandler', () => {
+  it('should return organizationId if user is an owner', async () => {
+    const organizationId = nanoid();
+    const userId = nanoid();
+    await setupTestEntities(organizationId, userId);
+
+    const query = createQuery({ userId });
+    const result = await queryBus.execute(query);
+
+    expect(result).toEqual(organizationId);
+  });
+
+  xit('should return undefined if user is not an owner', async () => {
+    const organizationId = nanoid();
+    const userId = nanoid();
+
+    const query = createQuery({ userId });
+    const result = await queryBus.execute(query);
+
+    expect(result).toEqual(organizationId);
+  });
+
+  let queryBus: QueryBus;
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [CqrsModule],
+      providers: [GetUserOrganizationHandler, PrismaService],
+    }).compile();
+
+    prismaService = module.get(PrismaService);
+    queryBus = module.get(QueryBus);
+
+    queryBus.register([GetUserOrganizationHandler as QueryHandlerType]);
+  });
+
+  const createQuery = (
+    data: Partial<GetUserOrganizationQuery['data']> = {},
+  ) => {
+    return new GetUserOrganizationQuery({
+      userId: 'userId',
+      ...data,
+    });
+  };
+
+  const setupTestEntities = async (
+    organizationId: string,
+    userId: string,
+  ): Promise<Organization> => {
+    await testCreateUser(prismaService, { id: userId });
+
+    const data: Prisma.UserOrganizationCreateInput = {
+      id: nanoid(),
+      role: {
+        connect: {
+          id: UserOrganizationRoles.organizationOwner,
+        },
+      },
+      organization: {
+        create: {
+          id: organizationId,
+        },
+      },
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+    };
+
+    return prismaService.userOrganization.create({
+      data,
+    });
+  };
+});
--- src/user/queries/handlers/get-projects-by-user-id.handler.ts
+++ src/user/queries/handlers/get-projects-by-user-id.handler.ts
@@ -1,12 +1,10 @@
-import { IQueryHandler, QueryBus, QueryHandler } from '@nestjs/cqrs';
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 import { getOffsetPagination } from 'src/share/commands/utils/getOffsetPagination';
-import { Prisma } from '@prisma/client';
 import {
   GetProjectsByUserIdQuery,
   GetProjectsByUserIdQueryReturnType,
-  GetUserOrganizationQuery,
-  GetUserOrganizationQueryReturnType,
 } from 'src/user/queries/impl';
 
 type WhereData = { userId: string; organizationId?: string };
@@ -16,21 +14,17 @@ export class GetProjectsByUserIdHandler
   implements
     IQueryHandler<GetProjectsByUserIdQuery, GetProjectsByUserIdQueryReturnType>
 {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly queryBus: QueryBus,
-  ) {}
+  constructor(private readonly prisma: PrismaService) {}
 
   public async execute({ data }: GetProjectsByUserIdQuery) {
     const whereData: WhereData = {
       userId: data.userId,
-      organizationId: await this.getUserOrganizationId(data.userId),
     };
 
     return getOffsetPagination({
       pageData: data,
       findMany: (args) => this.getProjects(this.getWhereInput(whereData), args),
-      count: () => this.getProjectsCount(this.getWhereInput(data)),
+      count: () => this.getProjectsCount(this.getWhereInput(whereData)),
     });
   }
 
@@ -77,12 +71,5 @@ export class GetProjectsByUserIdHandler
     return {
       OR,
     };
-  }
-
-  private getUserOrganizationId(userId: string) {
-    return this.queryBus.execute<
-      GetUserOrganizationQuery,
-      GetUserOrganizationQueryReturnType
-    >(new GetUserOrganizationQuery({ userId }));
   }
 }
