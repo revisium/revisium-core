268740ef2dc6001d5c5363ba785021dd70bb59e3
Anton Kashirov <anton.kashirov@gmail.com>
2025-01-04 17:53:53 +0300

    chore: updated create-row.spec.ts

--- src/draft/commands/handlers/__tests__/create-row.spec.ts
+++ src/draft/commands/handlers/__tests__/create-row.spec.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@prisma/client';
 import { BadRequestException } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { PrismaService } from 'src/database/prisma.service';
@@ -6,10 +7,12 @@ import {
   createMock,
   createTestingModule,
   prepareBranch,
+  PrepareBranchReturnType,
 } from 'src/draft/commands/handlers/__tests__/utils';
 import { CreateRowCommand } from 'src/draft/commands/impl/create-row.command';
 import { CreateRowHandlerReturnType } from 'src/draft/commands/types/create-row.handler.types';
 import { DraftTransactionalCommands } from 'src/draft/draft.transactional.commands';
+import { SystemTables } from 'src/share/system-tables.consts';
 
 describe('CreateRowHandler', () => {
   it('should throw an error if the rowId is shorter than 1 character', async () => {
@@ -60,6 +63,160 @@ describe('CreateRowHandler', () => {
       'A row with this name already exists in the table',
     );
   });
+
+  it('should throw an error if the data is not valid', async () => {
+    const { draftRevisionId, tableId, rowId } =
+      await prepareBranch(prismaService);
+
+    const command = new CreateRowCommand({
+      revisionId: draftRevisionId,
+      tableId: tableId,
+      rowId: rowId,
+      data: { test: 1 },
+    });
+
+    await expect(runTransaction(command)).rejects.toThrow('data is not valid');
+  });
+
+  it('should throw an error if the table is a system table', async () => {
+    const { draftRevisionId, rowId } = await prepareBranch(prismaService);
+
+    const command = new CreateRowCommand({
+      revisionId: draftRevisionId,
+      tableId: SystemTables.Schema,
+      rowId: rowId,
+      data: { test: 'value' },
+    });
+
+    await expect(runTransaction(command)).rejects.toThrow('data is not valid');
+  });
+
+  it('should create a new row if conditions are met', async () => {
+    const ids = await prepareBranch(prismaService);
+    const { draftRevisionId, tableId, draftTableVersionId } = ids;
+
+    const command = new CreateRowCommand({
+      revisionId: draftRevisionId,
+      tableId: tableId,
+      rowId: 'newRowId',
+      data: { test: 'value' },
+    });
+
+    const result = await runTransaction(command);
+
+    expect(result.tableVersionId).toBe(draftTableVersionId);
+    expect(result.previousTableVersionId).toBe(draftTableVersionId);
+    expect(result.rowVersionId).toBeTruthy();
+
+    await rowCheck(
+      ids,
+      command.data.rowId,
+      result.rowVersionId,
+      command.data.data,
+    );
+    await changelogCheck(ids, command.data.rowId);
+  });
+
+  it('should create a new row if conditions are met if the table is a system table and skipCheckingNotSystemTable = true', async () => {
+    const ids = await prepareBranch(prismaService);
+    const { draftRevisionId } = ids;
+
+    const command = new CreateRowCommand({
+      revisionId: draftRevisionId,
+      tableId: SystemTables.Schema,
+      skipCheckingNotSystemTable: true,
+      rowId: 'newRowId',
+      data: {
+        type: 'object',
+        required: ['test'],
+        properties: {
+          test: {
+            type: 'string',
+            default: '',
+          },
+        },
+        additionalProperties: false,
+      },
+    });
+
+    const result = await runTransaction(command);
+
+    expect(result).toBeTruthy();
+  });
+
+  it('should create a new row in a new created table if conditions are met', async () => {
+    const ids = await prepareBranch(prismaService);
+    const { draftRevisionId, tableId, draftTableVersionId } = ids;
+    await prismaService.table.update({
+      where: {
+        versionId: draftTableVersionId,
+      },
+      data: {
+        readonly: true,
+      },
+    });
+
+    const command = new CreateRowCommand({
+      revisionId: draftRevisionId,
+      tableId: tableId,
+      rowId: 'newRowId',
+      data: { test: 'value' },
+    });
+
+    const result = await runTransaction(command);
+
+    expect(result.tableVersionId).not.toBe(draftTableVersionId);
+    expect(result.previousTableVersionId).toBe(draftTableVersionId);
+    expect(result.rowVersionId).toBeTruthy();
+  });
+
+  async function rowCheck(
+    ids: PrepareBranchReturnType,
+    rowId: string,
+    createdRowVersionId: string,
+    data: Prisma.InputJsonValue,
+  ) {
+    const { draftRevisionId, tableId } = ids;
+
+    const row = await prismaService.row.findFirstOrThrow({
+      where: {
+        id: rowId,
+        tables: {
+          some: {
+            id: tableId,
+            revisions: {
+              some: {
+                id: draftRevisionId,
+              },
+            },
+          },
+        },
+      },
+    });
+    expect(row.id).toBe(rowId);
+    expect(row.versionId).toBe(createdRowVersionId);
+    expect(row.data).toStrictEqual(data);
+    expect(row.readonly).toBe(false);
+  }
+
+  async function changelogCheck(ids: PrepareBranchReturnType, rowId: string) {
+    const { draftChangelogId, tableId } = ids;
+
+    const changelog = await prismaService.changelog.findFirstOrThrow({
+      where: {
+        id: draftChangelogId,
+      },
+    });
+    expect(changelog.hasChanges).toBe(true);
+    expect(changelog.rowInsertsCount).toBe(1);
+    expect(changelog.rowInserts).toStrictEqual({
+      [tableId]: {
+        rows: {
+          [rowId]: '',
+        },
+      },
+    });
+  }
 
   function runTransaction(
     command: CreateRowCommand,
