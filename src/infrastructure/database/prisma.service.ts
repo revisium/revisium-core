dbf8e33745019a67e4ec42169dd03fa891118fa7
Anton Kashirov <anton.kashirov@gmail.com>
2025-04-08 08:04:22 +0300

    revert

--- src/infrastructure/database/prisma.service.ts
+++ src/infrastructure/database/prisma.service.ts
@@ -3,42 +3,7 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      // log: ['query'],
-    });
-  }
-
   async onModuleInit() {
     await this.$connect();
-
-    console.log('test1');
-    const res1 = await this.row.findMany({
-      where: { id: '1' },
-      select: {
-        createdId: true,
-        versionId: true,
-      },
-    });
-    console.log('test2');
-    const res2 = await this.row.findMany({
-      where: {
-        tables: {
-          some: {
-            createdId: 'tableId',
-            revisions: {
-              some: {
-                id: 'rev1',
-              },
-            },
-          },
-        },
-      },
-      select: {
-        createdId: true,
-        versionId: true,
-      },
-    });
-    console.log('test3');
   }
 }
