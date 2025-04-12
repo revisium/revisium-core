3cde15021f58dfadf31ed1eced71a91839f87bbd
Anton Kashirov <anton.kashirov@gmail.com>
2025-04-12 12:50:02 +0300

    getInvalidFieldNamesInSchema

---
+++ src/features/share/utils/schema/lib/getInvalidFieldNamesInSchema.ts
@@ -0,0 +1,26 @@
+import { createJsonSchemaStore } from 'src/features/share/utils/schema/lib/createJsonSchemaStore';
+import { traverseStore } from 'src/features/share/utils/schema/lib/traverseStore';
+import { JsonSchemaStore } from 'src/features/share/utils/schema/model/schema/json-schema.store';
+import {
+  JsonSchema,
+  JsonSchemaTypeName,
+} from 'src/features/share/utils/schema/types/schema.types';
+import { validateJsonFieldName } from 'src/features/share/utils/validateUrlLikeId/validateJsonFieldName';
+
+export const getInvalidFieldNamesInSchema = (schema: JsonSchema) => {
+  const schemaStore = createJsonSchemaStore(schema);
+
+  const invalidFields: JsonSchemaStore[] = [];
+
+  traverseStore(schemaStore, (item) => {
+    if (item.parent?.type === JsonSchemaTypeName.Object) {
+      try {
+        validateJsonFieldName(item.name);
+      } catch (e) {
+        invalidFields.push(item);
+      }
+    }
+  });
+
+  return invalidFields;
+};
