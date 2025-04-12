61c877b3babc38ee585822828f232604c7480698
Anton Kashirov <anton.kashirov@gmail.com>
2025-04-12 14:03:17 +0300

    refactor validateJsonFieldName

--- src/features/share/utils/validateUrlLikeId/validateJsonFieldName.ts
+++ src/features/share/utils/validateUrlLikeId/validateJsonFieldName.ts
@@ -3,10 +3,10 @@ import { VALIDATE_URL_LIKE_ID_ERROR_MESSAGE } from 'src/features/share/utils/val
 export const VALIDATE_JSON_FIELD_NAME_ERROR_MESSAGE =
   VALIDATE_URL_LIKE_ID_ERROR_MESSAGE;
 
-export const validateJsonFieldName = (id: string) => {
-  const validPattern = /^(?!__)[a-zA-Z_][a-zA-Z0-9-_]*$/;
-  const maxLength = 64;
+const validPattern = /^(?!__)[a-zA-Z_][a-zA-Z0-9-_]*$/;
+const maxLength = 64;
 
+export const validateJsonFieldName = (id: string) => {
   const isInvalid =
     id.length < 1 || id.length > maxLength || !validPattern.test(id);
 
