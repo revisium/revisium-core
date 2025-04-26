57c7a1d5b2fc29176a87dd5973ee28d2b479bff1
Anton Kashirov <anton.kashirov@gmail.com>
2025-04-26 18:31:53 +0300

    updated test

--- src/features/draft/commands/handlers/__tests__/update-row.spec.ts
+++ src/features/draft/commands/handlers/__tests__/update-row.spec.ts
@@ -210,15 +210,16 @@ describe('UpdateRowHandler', () => {
     });
 
     const file = {
-      status: 'ready',
-      url: 'url',
-      filename: 'filename.png',
-      hash: 'hash',
-      extension: 'png',
-      mimeType: 'mimeType',
-      size: 12345,
-      width: 1000,
-      height: 2000,
+      status: '',
+      fileId: '',
+      url: '',
+      filename: '',
+      hash: '',
+      extension: '',
+      mimeType: '',
+      size: 0,
+      width: 0,
+      height: 0,
     };
 
     const command = new UpdateRowCommand({
