fa0fc59f50f5659ec0559b56589ccb96b663c705
Anton Kashirov <anton.kashirov@gmail.com>
2025-04-26 18:06:09 +0300

    updateRow logic

--- src/features/plugin/file.plugin.ts
+++ src/features/plugin/file.plugin.ts
@@ -6,7 +6,6 @@ import {
   IPluginService,
 } from 'src/features/plugin/types';
 import { SystemSchemaIds } from 'src/features/share/schema-ids.consts';
-import { getDBJsonPathByJsonSchemaStore } from 'src/features/share/utils/schema/lib/getDBJsonPathByJsonSchemaStore';
 import { traverseValue } from 'src/features/share/utils/schema/lib/traverseValue';
 import { JsonObjectValueStore } from 'src/features/share/utils/schema/model/value/json-object-value.store';
 import { JsonStringValueStore } from 'src/features/share/utils/schema/model/value/json-string-value.store';
@@ -15,7 +14,7 @@ import { JsonSchemaTypeName } from 'src/features/share/utils/schema/types/schema
 export enum FileStatus {
   ready = 'ready',
   error = 'error',
-  completed = 'completed',
+  completed = 'uploaded',
 }
 
 @Injectable()
@@ -51,16 +50,57 @@ export class FilePlugin implements IPluginService {
   }
 
   public async updateRow(options: InternalUpdateRowOptions): Promise<void> {
-    traverseValue(options.valueStore, (item) => {
+    const previousFiles: Map<string, JsonObjectValueStore> = new Map();
+
+    traverseValue(options.previousValueStore, (item) => {
       if (item.schema.$ref === SystemSchemaIds.File) {
         if (item.type === JsonSchemaTypeName.Object) {
-          console.log('updateRow', getDBJsonPathByJsonSchemaStore(item.schema));
-          // this.checkDefaultValues(item);
-          // this.prepareFile(item);
+          const fileIdStore = item.value['fileId'] as JsonStringValueStore;
+          const fileId = fileIdStore.getPlainValue();
+          previousFiles.set(fileId, item);
         } else {
           throw new Error('Invalid schema type');
         }
       }
     });
+
+    traverseValue(options.valueStore, (item) => {
+      if (item.schema.$ref === SystemSchemaIds.File) {
+        if (item.type === JsonSchemaTypeName.Object) {
+          const fileIdStore = item.value['fileId'] as JsonStringValueStore;
+          const fileId = fileIdStore.getPlainValue();
+          const previousFile = previousFiles.get(fileId);
+
+          if (fileId) {
+            if (!previousFile) {
+              throw new Error(`File ${fileId} does not exist`);
+            }
+
+            this.checkUpdatedFile(item, previousFile);
+          } else {
+            this.checkDefaultValues(item);
+            this.prepareFile(item);
+          }
+        } else {
+          throw new Error('Invalid schema type');
+        }
+      }
+    });
+  }
+
+  private checkUpdatedFile(
+    file: JsonObjectValueStore,
+    previousFile: JsonObjectValueStore,
+  ) {
+    for (const [field, item] of Object.entries(file.value)) {
+      if (
+        field !== 'filename' &&
+        item.value !== previousFile.value[field].value
+      ) {
+        throw new Error(
+          `${field} must have value = ${previousFile.value[field].value}`,
+        );
+      }
+    }
   }
 }
