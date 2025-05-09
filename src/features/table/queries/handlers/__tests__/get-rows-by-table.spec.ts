9649e9ba7e30c4fa5804b6bf395c356b7e64ef54
Anton Kashirov <anton.kashirov@gmail.com>
2025-05-09 17:53:54 +0300

    added get rows by table test

---
+++ src/features/table/queries/handlers/__tests__/get-rows-by-table.spec.ts
@@ -0,0 +1,63 @@
+import { QueryBus } from '@nestjs/cqrs';
+import {
+  createPreviousFile,
+  prepareTableAndRowWithFile,
+} from 'src/__tests__/utils/prepareProject';
+import { createTestingModule } from 'src/features/draft/commands/handlers/__tests__/utils';
+import { FileStatus } from 'src/features/plugin/file/file.plugin';
+import { GetRowsByTableQuery } from 'src/features/table/queries/impl/get-rows-by-table.query';
+import { GetTableRowsReturnType } from 'src/features/table/queries/types';
+import { PrismaService } from 'src/infrastructure/database/prisma.service';
+import { TransactionPrismaService } from 'src/infrastructure/database/transaction-prisma.service';
+
+describe('getRowsByTable', () => {
+  it('should compute rows', async () => {
+    const data = {
+      file: {
+        ...createPreviousFile(),
+        status: FileStatus.uploaded,
+        url: '',
+      },
+      files: [],
+    };
+
+    const { draftRevisionId, table } = await prepareTableAndRowWithFile(
+      prismaService,
+      data,
+    );
+
+    const result = await runTransaction(
+      new GetRowsByTableQuery({
+        revisionId: draftRevisionId,
+        tableId: table.tableId,
+        tableVersionId: table.draftTableVersionId,
+        first: 1,
+      }),
+    );
+
+    const resultData = result.edges[0].node.data as typeof data;
+
+    expect(resultData.file.url).toBeTruthy();
+  });
+
+  function runTransaction(
+    query: GetRowsByTableQuery,
+  ): Promise<GetTableRowsReturnType> {
+    return transactionService.run(async () => queryBus.execute(query));
+  }
+
+  let prismaService: PrismaService;
+  let transactionService: TransactionPrismaService;
+  let queryBus: QueryBus;
+
+  beforeAll(async () => {
+    const result = await createTestingModule();
+    prismaService = result.prismaService;
+    transactionService = result.transactionService;
+    queryBus = result.queryBus;
+  });
+
+  afterAll(async () => {
+    await prismaService.$disconnect();
+  });
+});
