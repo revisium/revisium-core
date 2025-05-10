8b6d4d1bbc69568d23d24befbc887760bb62a3dd
Anton Kashirov <anton.kashirov@gmail.com>
2025-05-10 10:03:44 +0300

    feat: reserved names (#139)

--- src/features/share/system-organizations.consts.ts
+++ src/features/share/system-organizations.consts.ts
@@ -2,4 +2,11 @@ export enum SystemOrganizations {
   Revisium = 'revisium',
   Sandbox = 'sandbox',
   Playground = 'playground',
+  Api = 'api',
+  Graphql = 'graphql',
+  Json = 'json',
+  Schema = 'schema',
+  JsonSchema = 'json-schema',
+  Content = 'content',
+  Data = 'data',
 }
