ebd763080f11ee7a5ca0ac62787722fe1eb491aa
Anton Kashirov <anton.kashirov@gmail.com>
2025-05-14 09:30:55 +0300

    feat: init order by

--- src/api/rest-api/table/dto/get-table-rows.dto.ts
+++ src/api/rest-api/table/dto/get-table-rows.dto.ts
@@ -1,6 +1,34 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { Transform } from 'class-transformer';
-import { IsInt, Min } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform, Type } from 'class-transformer';
+import {
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Min,
+  ValidateNested,
+} from 'class-validator';
+
+export enum SortDirection {
+  asc = 'asc',
+  desc = 'desc',
+}
+
+export class OrderByDto {
+  @ApiProperty({
+    enum: ['id', 'createdAt', 'name'],
+    description: 'Поле для сортировки',
+  })
+  @IsString()
+  field: 'id' | 'createdAt' | 'name';
+
+  @ApiProperty({
+    enum: SortDirection,
+    description: 'Направление сортировки',
+  })
+  @IsEnum(SortDirection)
+  direction: SortDirection;
+}
 
 export class GetTableRowsDto {
   @ApiProperty({ default: 100 })
@@ -11,4 +39,35 @@ export class GetTableRowsDto {
 
   @ApiProperty({ required: false })
   after?: string;
+
+  @ApiPropertyOptional({
+    type: 'object',
+    properties: {
+      orderBy: {
+        type: 'array',
+        example: [{ createdAt: 'asc' }],
+        items: {
+          type: 'object',
+        },
+      },
+    },
+    description: 'Массив критериев сортировки',
+  })
+  @Transform(({ value }) => {
+    console.log('value', value, typeof value);
+    console.log(
+      'parsed',
+      typeof value === 'string' ? JSON.parse(value) : value,
+    );
+    try {
+      const parsed = typeof value === 'string' ? JSON.parse(value) : value;
+      return Array.isArray(parsed) ? parsed : [parsed];
+    } catch (e) {
+      throw new Error('Invalid JSON in orderBy');
+    }
+  })
+  @IsOptional()
+  @ValidateNested({ each: true })
+  @Type(() => OrderByDto)
+  orderBy?: OrderByDto;
 }
