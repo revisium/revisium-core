ea808b3907c30b2d713ee9152ddaf78f6a8e1b0f
Anton Kashirov <anton.kashirov@gmail.com>
2025-07-15 19:07:02 +0300

    resolve comments

--- src/features/auth/guards/jwt/optional-http-jwt-auth-guard.service.ts
+++ src/features/auth/guards/jwt/optional-http-jwt-auth-guard.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class OptionalHttpJwtAuthGuard extends AuthGuard('jwt') {
-  constructor(private readonly jwtService: JwtService) {
-    super();
-  }
-
   handleRequest(_: any, user: any) {
     return user;
   }
