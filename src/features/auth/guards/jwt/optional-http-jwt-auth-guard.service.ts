a0fe50264ad8002114336b29a2080886f77777ef
Anton Kashirov <anton.kashirov@gmail.com>
2025-07-15 19:12:01 +0300

    feat: optional http guard (#196)

--- src/features/auth/guards/jwt/optional-http-jwt-auth-guard.service.ts
+++ src/features/auth/guards/jwt/optional-http-jwt-auth-guard.service.ts
@@ -1,16 +1,9 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class OptionalHttpJwtAuthGuard extends AuthGuard('jwt') {
-  canActivate(context: ExecutionContext) {
-    const request = context.switchToHttp().getRequest();
-
-    const authHeader = request.headers['authorization'];
-    if (!authHeader) {
-      return true;
-    }
-
-    return super.canActivate(context);
+  handleRequest(_: any, user: any) {
+    return user;
   }
 }
