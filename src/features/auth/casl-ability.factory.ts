3ccea2862ae5f73038bce47b544e6fdf370da369
Anton Kashirov <anton.kashirov@gmail.com>
2025-09-13 15:11:16 +0300

    feat: casl caching

--- src/features/auth/casl-ability.factory.ts
+++ src/features/auth/casl-ability.factory.ts
@@ -2,6 +2,7 @@ import { AbilityBuilder, createMongoAbility, MongoQuery } from '@casl/ability';
 import { AnyObject } from '@casl/ability/dist/types/types';
 import { Injectable } from '@nestjs/common';
 import { UserRole } from 'src/features/auth/consts';
+import { CachedMethod } from 'src/infrastructure/cache-manager/method-cache.decorator';
 import { PrismaService } from 'src/infrastructure/database/prisma.service';
 
 @Injectable()
@@ -22,6 +23,15 @@ export class CaslAbilityFactory {
     return build();
   }
 
+  @CachedMethod<
+    UserRole,
+    Array<{ action: string; subject: string; condition?: object }>
+  >({
+    keyPrefix: 'role:permissions',
+    makeKey: ([role]) => role,
+    makeTags: ([role]) => ['dict:roles', `role:${role}`],
+    ttlSec: 24 * 60 * 60,
+  })
   private getPermissions(role: UserRole) {
     return this.prisma.role
       .findUniqueOrThrow({
