6e7c445a046e7cbc9c566e7b265c5367ee66d3ee
Anton Kashirov <anton.kashirov@gmail.com>
2025-10-16 20:45:45 +0300

    fix: RevisionRevertedEventHandler (#264)

--- src/infrastructure/cache/handlers/revision/revision-reverted.handler.ts
+++ src/infrastructure/cache/handlers/revision/revision-reverted.handler.ts
@@ -1,14 +1,21 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { RevisionRevertedEvent } from 'src/infrastructure/cache/events';
 import { RevisionCacheService } from 'src/infrastructure/cache/services/revision-cache.service';
+import { RowCacheService } from 'src/infrastructure/cache/services/row-cache.service';
 
 @EventsHandler(RevisionRevertedEvent)
 export class RevisionRevertedEventHandler
   implements IEventHandler<RevisionRevertedEvent>
 {
-  constructor(private readonly revisionCache: RevisionCacheService) {}
+  constructor(
+    private readonly revisionCache: RevisionCacheService,
+    private readonly rowCache: RowCacheService,
+  ) {}
 
   async handle(event: RevisionRevertedEvent) {
-    await this.revisionCache.invalidateRevisions([event.revisionId]);
+    await Promise.all([
+      this.revisionCache.invalidateRevisions([event.revisionId]),
+      this.rowCache.invalidateRevisionRelatives(event.revisionId),
+    ]);
   }
 }
