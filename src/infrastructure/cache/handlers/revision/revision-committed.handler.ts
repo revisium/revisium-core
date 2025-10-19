85c5affb95354c1a6292f90cbec2177657100e86
Anton Kashirov <anton.kashirov@gmail.com>
2025-10-19 10:55:41 +0300

    fix: revision committed (#265)

--- src/infrastructure/cache/handlers/revision/revision-committed.handler.ts
+++ src/infrastructure/cache/handlers/revision/revision-committed.handler.ts
@@ -1,17 +1,24 @@
 import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { RevisionCommittedEvent } from 'src/infrastructure/cache/events';
 import { RevisionCacheService } from 'src/infrastructure/cache/services/revision-cache.service';
+import { RowCacheService } from 'src/infrastructure/cache/services/row-cache.service';
 
 @EventsHandler(RevisionCommittedEvent)
 export class RevisionCommittedEventHandler
   implements IEventHandler<RevisionCommittedEvent>
 {
-  constructor(private readonly revisionCache: RevisionCacheService) {}
+  constructor(
+    private readonly revisionCache: RevisionCacheService,
+    private readonly rowCache: RowCacheService,
+  ) {}
 
   async handle(event: RevisionCommittedEvent) {
-    await this.revisionCache.invalidateRevisions([
-      event.previousHeadRevisionId,
-      event.previousDraftRevisionId,
+    await Promise.all([
+      this.revisionCache.invalidateRevisions([
+        event.previousHeadRevisionId,
+        event.previousDraftRevisionId,
+      ]),
+      this.rowCache.invalidateRevisionRelatives(event.previousDraftRevisionId),
     ]);
   }
 }
